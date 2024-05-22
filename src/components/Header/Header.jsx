3a9d49d94759888a03bc54250b402fe4f5df1b94
Your felixaveiro <bikofelix2020@gmail.com>
2024-05-22 21:23:18 +0200

    updating the page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,40 @@
 import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
 import { IoCall } from "react-icons/io5";
-
+import { MdClose } from "react-icons/md";
+import { MdMenu } from "react-icons/md";
 import image from '../../assets/LOGO - Avocado Society of Rwanda.png'
 import './Header.css'
 
 
 function Header() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
   return (
     <>
-    <div className="header">
+      <div className="header">
         <div>
-            <Link to="/"> <img className='logo' src={image} alt="" /></Link>
+          <Link to="/"> <img className='logo' src={image} alt="" /></Link>
         </div>
-        <div className='header-nav'>
-            <ul>
-               <li><Link to="/">Home</Link></li>
-               <li><Link to="/About">About</Link></li>
-               <li><Link to="/Membership">Membership</Link></li>
-               <div className='icon'>
-          <IoCall className='icon1'/>
-          <p>+250780941222</p>
-        </div>
-            </ul>
+        <div className={`header-nav ${sidebarOpen ? 'open' : ''}`}>
+          <button onClick={() => setSidebarOpen(false)}><MdClose className='close' /></button>
+          <ul>
+            <li><Link to="/" onClick={closeSidebar}>Home</Link></li>
+            <li><Link to="/About" onClick={closeSidebar}>About</Link></li>
+            <li><Link to="/Membership" onClick={closeSidebar}>Membership</Link></li>
+            <div className='icon'>
+              <IoCall className='icon1' />
+              <p>+250780941222</p>
+            </div>
+          </ul>
         </div>
         <div>
-            <div className='header-button'>Get in touch</div>
+          <div className='header-button'>Get in touch</div>
         </div>
-     </div>   
+        <button className="menu-button" onClick={() => setSidebarOpen(true)}><MdMenu /></button>
+      </div>
     </>
   )
 }
